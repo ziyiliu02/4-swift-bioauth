73ce9553fa336e4573ef8edc840c84f5c557d539
Liu Ziyi <ziyi.liu02@gmail.com>
2023-05-05 23:08:02 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Liu Ziyi on 5/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Biometrics Authentication")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/__swift_bioauthApp.swift
@@ -0,0 +1,17 @@
+//
+//  __swift_bioauthApp.swift
+//  Shared
+//
+//  Created by Liu Ziyi on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct __swift_bioauthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
