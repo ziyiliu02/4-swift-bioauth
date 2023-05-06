57ab10c9169e0bc4d3c5c2d06c8c7c1bd9be4523
Liu Ziyi <ziyi.liu02@gmail.com>
2023-05-06 18:11:49 +0800

    - Get biometryType

--- Shared/AuthenticationManager.swift
+++ Shared/AuthenticationManager.swift
@@ -10,4 +10,16 @@ import LocalAuthentication
 
 class AuthenticationManager: ObservableObject {
     private(set) var context = LAContext()
+    @Published private(set) var biometryType: LABiometryType = .none
+    private(set) var canEvaluatePolicy = false
+    
+    init() {
+        getBiometryType()
+    }
+    
+    func getBiometryType() {
+        canEvaluatePolicy = context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil)
+        biometryType = context.biometryType
+    }
+    
 }
