c3a3e046fe26bba72cedf8c5fde41437015acfd1
Liu Ziyi <ziyi.liu02@gmail.com>
2023-05-06 17:45:27 +0800

    - Create the AuthenticationManager

---
+++ Shared/AuthenticationManager.swift
@@ -0,0 +1,13 @@
+//
+//  AuthenticationManager.swift
+//  4-swift-bioauth (iOS)
+//
+//  Created by Liu Ziyi on 6/5/23.
+//
+
+import Foundation
+import LocalAuthentication
+
+class AuthenticationManager: ObservableObject {
+    private(set) var context = LAContext()
+}
